cd9a75684f6d0b1597d403b7b6d0b6a6a61b3cb4
Vipin Chandran P <vipin.chandranp@gmail.com>
2015-10-24 21:13:51 +0530

    1

--- src/main/java/com/priski/controller/PersonController.java
+++ src/main/java/com/priski/controller/PersonController.java
@@ -25,7 +25,7 @@ public class PersonController {
         map.put("person", new Person());
         map.put("peopleList", personService.listPeople());
 
-        return "people";
+        return "people"; 
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
